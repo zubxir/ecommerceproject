05009dc2b1addfaeeb5bc02242f3920bf6647ca3
zubxir <zoomohammmed@gmail.com>
2019-11-20 14:35:54 -0600

    rubocop added

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,9 @@ Rails.application.routes.draw do
   get '/products/:id', to: 'products#show', id: /\d+/
   get '/category', to: 'category#index'
   get '/category/:id', to: 'category#show', id: /\d+/
+  get '/', to: 'welcome#index'
 
-  root to: 'welcome#index'
+  root to: 'products#index'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
